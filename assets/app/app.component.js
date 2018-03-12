268c3214ebe0d83d55d06599c70feabb3984c8e1
unknown <im_tanzeel@yahoo.co.in>
2018-03-12 06:27:44 +0400

    first commit

---
+++ assets/app/app.component.js
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+    }
+    AppComponent.decorators = [
+        { type: Component, args: [{
+                    selector: 'my-app',
+                    templateUrl: './app.component.html',
+                    styleUrls: ['./app.component.css']
+                },] },
+    ];
+    /** @nocollapse */
+    AppComponent.ctorParameters = function () { return []; };
+    return AppComponent;
+}());
+export { AppComponent };
